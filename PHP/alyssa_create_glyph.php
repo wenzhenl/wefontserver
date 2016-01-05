d49f2bc77900438b26425ada335936d2204dc36d
Henry Dong <henrydyc@hotmail.com>
2016-01-04 22:51:15 -0800

    glyph and font path added

--- PHP/alyssa_create_glyph.php
+++ PHP/alyssa_create_glyph.php
@@ -61,7 +61,8 @@ if(!($stmt4_result = mysqli_query($conn, $stmt4)) ) rollback_and_exit($conn, 'DB
 //Step 3: Prepare glyph image and store to disk
 $user_glyph_id = mysqli_fetch_array($stmt4_result, MYSQLI_ASSOC)['glyph_id'];
 if ($user_image->content_type != "image/jpeg") exit_with_error('not a JPEG image');
-$path = ALYSSA_USER_PATH.'/'.$user_id.'/'.$user_fontname.'/'.$user_glyph_id.'.jpeg';
+$image_path = ALYSSA_USER_PATH.'/'.$user_id.'/'.$user_font_id.'/'.$user_glyph_id.'.jpeg';
+$font_path  = ALYSSA_USER_PATH.'/'.$user_id.'/alyssafont.ttf';
 $user_image_data = base64_decode( str_replace(' ', '+', $user_image->file_data) );
 
 if(!file_put_contents($path, $user_image_data)) rollback_and_exit($conn, 'failed to write glyph image at path '.$path);
